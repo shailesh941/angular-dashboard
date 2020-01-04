f8c0099b70ca9a1694d7467400ac6abf35534ea1
Shailesh Kumar <30897581+shailesh941@users.noreply.github.com>
2020-01-04 14:27:08 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SideBarComponent } from './core/side-bar/side-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoreComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
